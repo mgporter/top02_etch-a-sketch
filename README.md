3d827c67486caf13f4fd5b54763bd30906fad636
mgporter <26415062+mgporter@users.noreply.github.com>
2023-06-02 09:01:31 -0400

    updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # top02_etch-a-sketch
 Etch-a-sketch project from https://www.theodinproject.com/lessons/foundations-etch-a-sketch
+
+Skills used:
+
+1. Dynamically set up a grid layout in html/css
+
+2. manipulate colors by extracting and recombining their rgb components, using regex
+
+3. Create DOM elements and add event listeners dynamically based on user input
+
--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const gridSizeInput = document.getElementById('grid-size');
 const gridContainer = document.getElementById('grid-container');
+const colorCircles = document.querySelectorAll('.color-circle');
 
 let gridSize = 16;
+let color = "red";
 
 const submitGridSize = function () {
     gridSize = gridSizeInput.value;
+    while (gridContainer.lastChild) {
+        gridContainer.removeChild(gridContainer.lastChild)
+    }
     createGrid(gridSize);
 }
 
@@ -12,22 +17,90 @@ const submitGridSize = function () {
 const createGrid = function (size) {
 
     const numOfCells = size**2;
-    const gridColumns = '1fr '; 
+    const gridColumns = '1fr '.repeat(size);
 
-    gridContainer.style.setProperty('--number-of-columns', '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr')
+    gridContainer.style.setProperty('--number-of-columns', gridColumns)
 
     for (let i = 0; i < numOfCells; i++) {
         
         const gridCell = document.createElement('div');
         gridCell.classList.add('grid-cell');
-        gridCell.addEventListener('mouseover', modifyGridCell);
+        gridCell.style.backgroundColor = 'rgb(255, 255, 255)'; // set initial color of cells
+        gridCell.addEventListener('mouseover', colorGridCell);
         gridContainer.appendChild(gridCell);
     }
 }
 
-const modifyGridCell = function(e) {
-    console.log(e);
-    this.style.backgroundColor = 'black';
+// Set the color of the paintbrush and also add the drop-shadow to the selected color
+const setColor = function (colorFromButton) {
+    color = colorFromButton;        // set color from the button's function call
+    let shadowColor;
+
+    switch (colorFromButton) {
+        case 'red':
+            shadowColor = '#ffa37dff';
+            break;
+        case 'yellow':
+            shadowColor = '#fdd117ff';
+            break;
+        case 'green':
+            shadowColor = '#7ef725ff';
+            break;
+        case 'blue':
+            shadowColor = '#62ddffff';
+            break;
+    }
+
+    colorCircles.forEach((item) => {
+        if (item.id != colorFromButton) {
+            item.style.setProperty('filter', `hue-rotate(${item.dataset.hue}deg) drop-shadow(0 0 0 #ffffff)`)
+        } else {
+            item.style.setProperty('filter', `hue-rotate(${item.dataset.hue}deg) drop-shadow(0 0 12px ${shadowColor})`)
+        }
+    })
+    
+}
+
+
+// calculate the new value for an individual color
+const subtractFromColor = function (oldColor) {
+    let colorWeight = 36;       // the weight tells us how much we want to color the cell on each mouseover
+    let newColor = oldColor - colorWeight
+    if (newColor < 0) {
+        return 0;
+    } else {
+        return newColor;
+    }
+}
+
+// calculate the cell's bg color based on the cell's current color and the current color setting
+const colorGridCell = function(e) {
+    let bgColor = this.style.backgroundColor.match(/rgb\((\d+), (\d+), (\d+)\)/i);
+    let red = bgColor[1];
+    let green = bgColor[2];
+    let blue = bgColor[3];
+
+    switch (color) {
+        case 'red':
+            green = subtractFromColor(green)
+            blue = subtractFromColor(blue)
+            break;
+        case 'yellow':
+            blue = subtractFromColor(blue)
+            break;
+        case 'green':
+            red = subtractFromColor(red)
+            blue = subtractFromColor(blue)
+            break;
+        case 'blue':
+            green = subtractFromColor(green)
+            red = subtractFromColor(red)
+            break;
+    }
+
+    bgColor = `rgb(${red}, ${green}, ${blue})`
+
+    this.style.backgroundColor = bgColor;
 }
 
 // const createGrid = function (size) {
